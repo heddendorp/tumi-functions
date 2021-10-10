58b137158128d47953e15f8e5726877507846db2
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-10-10 14:10:12 +0200

    Handle multiple pages

--- pdfConvert/index.ts
+++ pdfConvert/index.ts
@@ -1,13 +1,43 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions";
 import { fromBuffer } from "pdf2pic";
 import { ToBase64Response } from "pdf2pic/dist/types/toBase64Response";
+import sharp = require("sharp");
 
-const blobTrigger: AzureFunction = async function (context: Context, inputBlob: any): Promise<void> {
-    context.log("Blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", inputBlob.length, "Bytes");
-    const pdfResponse = (await fromBuffer(inputBlob,{width:425, height:600})(1, true))as ToBase64Response;
-    context.log(pdfResponse);
-    const imageBuffer = Buffer.from(pdfResponse.base64, 'base64');
-    context.bindings.imageBlob = imageBuffer;
+const blobTrigger: AzureFunction = async function (
+  context: Context,
+  inputBlob: any
+): Promise<void> {
+  context.log(
+    "Blob trigger function processed blob \n Name:",
+    context.bindingData.name,
+    "\n Blob Size:",
+    inputBlob.length,
+    "Bytes"
+  );
+  const pdfResponse = (await fromBuffer(inputBlob, {
+    width: 425,
+    height: 600,
+  }).bulk(-1, true)) as ToBase64Response[];
+  context.log(pdfResponse.length);
+  const image = await sharp({
+    create: {
+      width: 425,
+      height: 600 * pdfResponse.length,
+      channels: 3,
+      background: "white",
+    },
+  })
+    .composite(
+      pdfResponse.map(({ base64 }, index) => ({
+        input: Buffer.from(base64, "base64"),
+        top: index * 600,
+        left: 0,
+      }))
+    )
+    .png()
+    .toBuffer();
+  const imageBuffer = image;
+  context.bindings.imageBlob = imageBuffer;
 };
 
 export default blobTrigger;
