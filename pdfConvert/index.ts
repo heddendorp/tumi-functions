5785c0dccb36e26cc878512db46d104c783c0ce8
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-10-10 11:16:53 +0200

    correct based64 handling

--- pdfConvert/index.ts
+++ pdfConvert/index.ts
@@ -6,22 +6,8 @@ const blobTrigger: AzureFunction = async function (context: Context, inputBlob: 
     context.log("Blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", inputBlob.length, "Bytes");
     const pdfResponse = (await fromBuffer(inputBlob)(1, true))as ToBase64Response;
     context.log(pdfResponse);
-    const imageBuffer = decodeBase64Image(pdfResponse.base64);
-    context.bindings.imageBlob = imageBuffer.data;
+    const imageBuffer = Buffer.from(pdfResponse.base64, 'base64');
+    context.bindings.imageBlob = imageBuffer;
 };
-
-function decodeBase64Image(dataString: string) {
-    const matches = dataString.match(/^data:([A-Za-z-+/]+);base64,(.+)$/),
-      response = { type: undefined, data: undefined };
-  
-    if (matches.length !== 3) {
-      throw new Error('Invalid input string');
-    }
-  
-    response.type = matches[1];
-    response.data = Buffer.from(matches[2], 'base64');
-  
-    return response;
-  }
 
 export default blobTrigger;
