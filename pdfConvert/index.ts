b87c221a72393c2c86332d3567e5c94aede0194f
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-10-10 11:36:45 +0200

    set A4 format

--- pdfConvert/index.ts
+++ pdfConvert/index.ts
@@ -4,7 +4,7 @@ import { ToBase64Response } from "pdf2pic/dist/types/toBase64Response";
 
 const blobTrigger: AzureFunction = async function (context: Context, inputBlob: any): Promise<void> {
     context.log("Blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", inputBlob.length, "Bytes");
-    const pdfResponse = (await fromBuffer(inputBlob)(1, true))as ToBase64Response;
+    const pdfResponse = (await fromBuffer(inputBlob,{width:425, height:600})(1, true))as ToBase64Response;
     context.log(pdfResponse);
     const imageBuffer = Buffer.from(pdfResponse.base64, 'base64');
     context.bindings.imageBlob = imageBuffer;
