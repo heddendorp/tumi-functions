9661d53f4424f86639e2c1f13a714293e4374d9f
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-10-16 19:02:09 +0200

    add image preview function

---
+++ imagePreview/index.ts
@@ -0,0 +1,48 @@
+import { ComputerVisionClient } from "@azure/cognitiveservices-computervision";
+import { ApiKeyCredentials } from "@azure/ms-rest-js";
+import { AzureFunction, Context } from "@azure/functions";
+import sharp = require("sharp");
+
+const key = process.env.VISION_KEY;
+const endpoint = process.env.VISION_ENDPOINT;
+const computerVisionClient = new ComputerVisionClient(
+  new ApiKeyCredentials({ inHeader: { "Ocp-Apim-Subscription-Key": key } }),
+  endpoint
+);
+
+const blobTrigger: AzureFunction = async function (
+  context: Context,
+  inputBlob: any
+): Promise<void> {
+  context.log(
+    "Blob trigger function processed blob \n Name:",
+    context.bindingData.name,
+    "\n Blob Size:",
+    inputBlob.length,
+    "Bytes"
+  );
+  const image = sharp(inputBlob);
+  const metadata = await image.metadata();
+  const ratio = metadata.width / metadata.height;
+  const cols = ratio > 1.25 ? 2 : 1;
+  const rows = ratio < 0.75 ? 2 : 1;
+  const thumbnail = await computerVisionClient.generateThumbnail(
+    400 * cols,
+    400 * rows,
+    context.bindingData.url,
+    {
+      smartCropping: true,
+    }
+  );
+  const chunks = [];
+  const thumbnailStream = thumbnail.readableStreamBody;
+  thumbnailStream.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+  thumbnailStream.on("end", () => {
+    const buffer = Buffer.concat(chunks);
+    context.bindings.outputBlob = buffer;
+  });
+};
+
+export default blobTrigger;
